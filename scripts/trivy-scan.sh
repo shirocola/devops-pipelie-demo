87460d796e2f25f7f72a91e1affcfbd5120de5c8
shirocola <kitpivatpiromgit@gmail.com>
2024-09-05 23:49:43 +0700

    clear repo

--- scripts/trivy-scan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Trivy scan script
-echo "Starting Trivy scan..."
-docker pull gcr.io/$PROJECT_ID/devops-demo:${BUILD_ID}
-trivy image --exit-code 1 --severity HIGH,CRITICAL gcr.io/$PROJECT_ID/devops-demo:${BUILD_ID}
