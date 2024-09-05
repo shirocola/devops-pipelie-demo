fda14e19205263e4633bf426882c8870634c7caf
shirocola <kitpivatpiromgit@gmail.com>
2024-09-05 23:48:52 +0700

    clear repo

--- scripts/checkmarx-scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Running Checkmarx Scan..."
-checkmarx scan --project "DevOps Demo" --token "$CHECKMARX_TOKEN" --output scan-results.json
-if [ $? -ne 0 ]; then
-  echo "Checkmarx Scan failed"
-  exit 1
-fi
-echo "Checkmarx Scan completed successfully."
